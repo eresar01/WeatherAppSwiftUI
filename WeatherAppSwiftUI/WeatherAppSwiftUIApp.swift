26a24537f069795e29595c170d0e9fcde7a5c297
Yerem Sargsyan <eresar01@gmail.com>
2023-04-10 12:54:43 +0400

    Initial Commit

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Yerem Sargsyan on 10.04.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
